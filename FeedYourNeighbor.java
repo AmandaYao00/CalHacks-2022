48cb873375d6f4b4f8b403711d556ec3753a4ea2
AmandaYao00 <amanda.yao@berkeley.edu>
2022-10-15 22:32:04 -0700

    user class

--- FeedYourNeighbor.java
+++ FeedYourNeighbor.java
@@ -1,4 +1,14 @@
 public class FeedYourNeighbor {
     
     
+    /* Returns list of users who are within x mile radius. */ 
+    public List<User> findNearbyUsers() {
+        return new ArrayList();
+    }
+
+
+
+    public notifyRecipients() {
+
+    }
 }
